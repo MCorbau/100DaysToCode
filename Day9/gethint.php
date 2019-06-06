233193650ed57d43f2f1be5317c109aadb1c02b8
Martin Corbau <martincorbau@hotmail.com>
2019-06-06 21:58:42 +0200

    Getting the gethint stable

--- Day9/gethint.php
+++ Day9/gethint.php
@@ -3,6 +3,7 @@ require 'SQLConnection.php';
 $conn= new PDO("mysql:host=$base;dbname=$dbName", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // Array with names
+/*
 $a[] = "Anna";
 $a[] = "Brittany";
 $a[] = "Cinderella";
@@ -33,26 +34,37 @@ $a[] = "Elizabeth";
 $a[] = "Ellen";
 $a[] = "Wenche";
 $a[] = "Vicky";
+try{
+  foreach ($a as $names) {
+    echo $name;
+    $sql1 = "INSERT INTO `Name` VALUES ('$names')";
+    $res = $conn->exec($sql1);
+    echo 'Success';
+  }
+}
+catch(PDOException $e){
+  echo $sql . "<br>" . $e->getMessage();
+}
 
 // get the q parameter from URL
 $q = $_REQUEST["q"];
 $hint = "";
 $sql = 'select * from Name';
 foreach ($conn->query($sql) as $row) {
-  print_r( $row['Name']) . "\t";
+  print_r( $row['Name']."<br>");
 }
 
+*/
 
 
-/*
 
 // lookup all hints from array if $q is different from ""
 if ($q !== "") {
   $q = strtolower($q);
   $len=strlen($q);
   $n = $conn->exec("SELECT * FROM Name");
-  echo $n;
-  foreach($a as $name) {
+  foreach($conn->query($sql) as $row) {
+    $name=$row['Name'];
     if (stristr($q, substr($name, 0, $len))) {
       if ($hint === "") {
         $hint = $name;
@@ -64,5 +76,5 @@ if ($q !== "") {
 }
 
 // Output "no suggestion" if no hint was found or output correct values
-echo $hint === "" ? "no suggestion" : $hint;*/
+echo $hint === "" ? "no suggestion" : $hint;
 ?>
