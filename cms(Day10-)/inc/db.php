db8888afb52324c8eb4119ef4a9a3a15962aef2c
Martin <Martincorbau@hotmail.com>
2019-06-14 23:01:33 +0200

    changing frontend code

--- cms(Day10-)/inc/db.php
+++ cms(Day10-)/inc/db.php
@@ -1,18 +1,15 @@
 <?php ob_start();
 
-db_host = "localhost";
-db_user = "root";
-db_pass = "root";
-db_name = "cms";
+$base = "localhost";
+$username = "root";
+$password = "root";
+$dbName = "cms";
 
 
 
-$connection = mysqli_connect(DB_HOST, DB_USER,DB_PASS,DB_NAME);
-
-
-
-$query = "SET NAMES utf8";
-mysqli_query($connection,$query);
-
+$conn = new mysqli($base, $username, $password,$dbName);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
 ?>
